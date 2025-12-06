63f347e3b2621040d8697e31e5cf0d6afee87627
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-06 16:46:28 +0500

    SOME CODE ADD

--- index.js
+++ index.js
@@ -4,12 +4,22 @@ const authRoutes = require("./routes/authRoute.js");
 const loginRoutes = require("./routes/loginRoute.js");
 const productRoutes = require("./routes/productsRoute.js")
 const categoriesRoutes = require("./routes/categoriesRoutes.js")
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true
+  })
+);
 
 // Routes
 app.use("/api/auth", authRoutes);
