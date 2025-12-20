abc1e16ac0d0680b39fb938ca29fd190657e2cd8
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-20 15:46:42 +0500

    auth middleware add

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -5,7 +5,7 @@ const parser = require("../config/multer");
 const auth = require("../middleware/auth");
 
 // Routes
-router.post("/", parser.single("image"), productController.createProduct , auth); // Create with image
+router.post("/",  auth , parser.single("image"), productController.createProduct); // Create with image
 router.get("/", productController.getAllProducts);                          // Get all
 router.get("/:id", productController.getProductById);                       // Get single
 router.put("/:id", parser.single("image"), productController.updateProduct);// Update with optional image
