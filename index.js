53116e1ac479d926dd3ca6f892ccb919fcb88d79
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-20 16:35:40 +0500

    logout add

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.use(
     origin: ["http://localhost:3000"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
--- routes/logoutRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { logout } = require("../controllers/authController");
-
-router.post("/logout", logout);
-
-module.exports = router;
