3896fc30c86c6e43cae1901d8b6adc072fd71fe6
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-27 14:54:04 +0500

    GET USERS AND VENDOR API ADD

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { getAllUsers, getAllVendors } = require("../controllers/userController");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
-router.get("/users", getAllUsers);
-router.get("/vendors", getAllVendors);
+router.get("/users", auth, getAllUsers);
+router.get("/vendors", auth, getAllVendors);
 
 module.exports = router;
