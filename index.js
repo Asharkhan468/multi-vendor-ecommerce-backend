07778fd7ebcf5920a9b4d1ac93851427808a0454
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-27 14:13:13 +0500

    ERROR RESOLVED

--- index.js
+++ index.js
@@ -44,10 +44,4 @@ const connectDB = async () => {
 connectDB();
 
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
