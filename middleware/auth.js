e005dd35bcce6f824bc53d5175e073622256ac43
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-31 19:09:34 +0500

    SOME CODE MODIFIED

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,85 +1,44 @@
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/User");
-
-// const auth = async (req, res, next) => {
-//   try {
-//     const authHeader = req.headers.authorization;
-
-//     if (!authHeader || !authHeader.startsWith("Bearer ")) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "No token provided",
-//       });
-//     }
-
-//     const token = authHeader.split(" ")[1];
-
-//     // verify token
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     // find user from DB
-//     const user = await User.findById(decoded.id).select("-password");
-
-//     if (!user) {
-//       return res.status(401).json({
-//         success: false,
-//         message: "User not found",
-//       });
-//     }
-
-//     req.user = user;
-
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({
-//       success: false,
-//       message: "Invalid or expired token",
-//     });
-//   }
-// };
-
-// module.exports = auth;
 
 
 
-// const jwt = require("jsonwebtoken");
-// const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-// exports.auth = async (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization?.split(" ")[1];
+exports.auth = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-//     if (!token) {
-//       return res
-//         .status(401)
-//         .json({ success: false, message: "Not authorized" });
-//     }
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Not authorized" });
+    }
 
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-//     const user = await User.findById(decoded.id);
+    const user = await User.findById(decoded.id);
 
-//     if (!user) {
-//       return res
-//         .status(401)
-//         .json({ success: false, message: "User not found" });
-//     }
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User not found" });
+    }
 
   
-//     if (user.status === "inactive") {
-//       return res.status(403).json({
-//         success: false,
-//         message: "Admin has blocked your account",
-//         logout: true,
-//       });
-//     }
+    if (user.status === "inactive") {
+      return res.status(403).json({
+        success: false,
+        message: "Admin has blocked your account",
+        logout: true,
+      });
+    }
 
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     return res.status(401).json({
-//       success: false,
-//       message: "Invalid or expired token",
-//     });
-//   }
-// };
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+};
