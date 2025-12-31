6215e9f7cfebe476c49e13e29dd806d5269fb790
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-31 18:17:05 +0500

    SOME CODE MODIFIED

--- routes/accountBlockRoute.js
+++ routes/accountBlockRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { blockUser } = require("../controllers/accountBlock");
-const { auth } = require("../middleware/auth");
+const blockUser  = require("../controllers/accountBlock");
+const auth = require("../middleware/auth");
 
 const router = express.Router();
 
