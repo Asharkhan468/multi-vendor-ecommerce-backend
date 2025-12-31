552598d122a95e11da5c255164e0f434fff2ece2
Asharkhan468 <asharullah.khan2468@gmail.com>
2025-12-31 18:31:11 +0500

    SOME CODE MODIFIED

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const productController = require("../controllers/productController");
+const {
+  createProduct,
+  getVendorProducts,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
 const parser = require("../config/multer");
 const auth = require("../middleware/auth");
 
 // Routes
-router.post("/", auth, parser.single("image"), productController.createProduct);
-router.get("/", productController.getAllProducts);
-router.get("/vendorProducts" , auth , productController.getVendorProducts);
-router.put("/:id", auth, parser.single("image"), productController.updateProduct);
-router.delete("/:id", auth , productController.deleteProduct);
+router.post("/", auth, parser.single("image"), createProduct);
+router.get("/", getAllProducts);
+router.get("/vendorProducts", auth, getVendorProducts);
+router.put("/:id", auth, parser.single("image"), updateProduct);
+router.delete("/:id", auth, deleteProduct);
 
 module.exports = router;
