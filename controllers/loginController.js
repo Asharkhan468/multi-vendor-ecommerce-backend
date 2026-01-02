9a42560a8276a8fe8c28b45e8974d3ac43ce593f
Asharkhan468 <asharullah.khan2468@gmail.com>
2026-01-02 15:52:04 +0500

    LOGIN API MODIFIED

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,48 @@
+// const User = require("../models/User.js");
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+
+// exports.login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     // Check if user exists
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(400).json({ message: "Invalid email" });
+//     }
+
+//     // Compare password
+//     const isPasswordMatched = await bcrypt.compare(password, user.password);
+//     if (!isPasswordMatched) {
+//       return res.status(400).json({ message: "Invalid password" });
+//     }
+
+//     // Create JWT Token
+//     const token = jwt.sign(
+//       { id: user._id, role: user.role },
+//       process.env.JWT_SECRET,
+//       { expiresIn: "7d" }
+//     );
+
+//     // Response
+//     res.status(200).json({
+//       message: "Logged in successfully",
+//       token,
+//       user: {
+//         id: user._id,
+//         name: user.name,
+//         email: user.email,
+//         role: user.role,
+//       },
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: "Server error", error: error.message });
+//   }
+// };
+
+
+
 const User = require("../models/User.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -6,26 +51,33 @@ exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    // Check if user exists
+    // 1️⃣ Check user exists
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: "Invalid email" });
     }
 
-    // Compare password
+    // 🔴 CHECK USER STATUS
+    if (user.status === "inactive") {
+      return res.status(401).json({
+        message: "Your account has been blocked by admin",
+      });
+    }
+
+    // 2️⃣ Compare password
     const isPasswordMatched = await bcrypt.compare(password, user.password);
     if (!isPasswordMatched) {
       return res.status(400).json({ message: "Invalid password" });
     }
 
-    // Create JWT Token
+    // 3️⃣ Create JWT
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: "7d" }
     );
 
-    // Response
+    // 4️⃣ Response
     res.status(200).json({
       message: "Logged in successfully",
       token,
@@ -37,6 +89,10 @@ exports.login = async (req, res) => {
       },
     });
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
   }
 };
+
