93f766e9de6cd1fdf3b3e872cd2c351d5b60764b
Asharkhan468 <asharullah.khan2468@gmail.com>
2026-01-05 18:52:05 +0500

    PHOTO SEND TO FRONTEND

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,48 +1,3 @@
-// const User = require("../models/User.js");
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
-
-// exports.login = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     // Check if user exists
-//     const user = await User.findOne({ email });
-//     if (!user) {
-//       return res.status(400).json({ message: "Invalid email" });
-//     }
-
-//     // Compare password
-//     const isPasswordMatched = await bcrypt.compare(password, user.password);
-//     if (!isPasswordMatched) {
-//       return res.status(400).json({ message: "Invalid password" });
-//     }
-
-//     // Create JWT Token
-//     const token = jwt.sign(
-//       { id: user._id, role: user.role },
-//       process.env.JWT_SECRET,
-//       { expiresIn: "7d" }
-//     );
-
-//     // Response
-//     res.status(200).json({
-//       message: "Logged in successfully",
-//       token,
-//       user: {
-//         id: user._id,
-//         name: user.name,
-//         email: user.email,
-//         role: user.role,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(500).json({ message: "Server error", error: error.message });
-//   }
-// };
-
-
-
 const User = require("../models/User.js");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
@@ -86,6 +41,7 @@ exports.login = async (req, res) => {
         name: user.name,
         email: user.email,
         role: user.role,
+        photo: user.profilePhoto,
       },
     });
   } catch (error) {
@@ -95,4 +51,3 @@ exports.login = async (req, res) => {
     });
   }
 };
-
