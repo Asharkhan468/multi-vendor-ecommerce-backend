58e846ba416b4f01006fa92dc7175853e82c5932
Asharkhan468 <asharullah.khan2468@gmail.com>
2026-01-06 17:32:55 +0500

    DELETE USER API ADD

---
+++ controllers/deleteUser.js
@@ -0,0 +1,28 @@
+const User = require("../models/User");
+
+const deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const user = await User.findByIdAndDelete(userId);
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
+  }
+};
+
+module.exports = deleteUser;
