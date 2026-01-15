d569dd251e62c210c8f1cfafe9bec302bf0ae82b
Asharkhan468 <asharullah.khan2468@gmail.com>
2026-01-15 16:38:01 +0500

    SOME CODE ADD

--- routes/descriptionGeneratorRoutes.js
+++ routes/descriptionGeneratorRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const multer = require("multer");
+const parser = require("../config/multer");
 const { imageToText } = require("../controllers/descriptionGenerator"); 
 
-const router = express.Router();
-const upload = multer({ dest: "images/" });
 
-router.post("/imageToText", upload.single("image"), imageToText);
+const router = express.Router();
+
+router.post("/imageToText", parser.single("image"), imageToText);
 
 module.exports = router;
