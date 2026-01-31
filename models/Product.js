3e6403f33b103d538dad5f20801d04df1b00c74b
Asharkhan468 <asharullah.khan2468@gmail.com>
2026-01-31 19:09:29 +0500

    REVIEW API ADD

--- models/Product.js
+++ models/Product.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const reviewSchema = new mongoose.Schema({
   userName: { type: String, required: true },
-  userPhoto: { type: String, required: true },
+  userPhoto: { type: String },
   rating: { type: Number, required: true, min: 1, max: 5 },
   comment: { type: String, required: true },
   date: { type: Date, default: Date.now },
