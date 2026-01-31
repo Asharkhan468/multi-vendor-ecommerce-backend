733dc035a14d9b74069f8e6e88165f9d4a5005ea
Asharkhan468 <asharullah.khan2468@gmail.com>
2026-01-31 18:08:07 +0500

    REVIEW API ADD

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,25 +1,38 @@
 const Product = require("../models/Product");
 
-// Add review controller
 exports.addReview = async (req, res) => {
-  const { id } = req.params;       // Product ID
-  const { comment, rating } = req.body;  // User input
+  const { id } = req.params; 
+  const { comment, rating } = req.body; 
 
   try {
     const product = await Product.findById(id);
     if (!product) return res.status(404).json({ message: "Product not found" });
 
     const review = {
-      userName: req.user.name,      // Current logged-in user
-      userPhoto: req.user.photo,    // Current user photo
+      userName: req.user.name, 
+      userPhoto: req.user.photo, 
       rating,
-      comment
+      comment,
     };
 
-    product.reviews.push(review);   // Add review
+    product.reviews.push(review); 
     await product.save();
 
     res.status(200).json({ message: "Review added successfully", review });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+exports.getReviews = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const product = await Product.findById(id);
+    if (!product) return res.status(404).json({ message: "Product not found" });
+
+    res.status(200).json({ reviews: product.reviews });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error" });
--- routes/reviewroute.js
+++ routes/reviewroute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { addReview } = require("../controllers/reviewController");
+const { addReview , getReviews} = require("../controllers/reviewController");
 const auth = require("../middleware/auth");
 
-
 router.post("/products/:id/review", auth, addReview);
+router.get("/product/:id/reviews", getReviews);
+
 
 module.exports = router;
